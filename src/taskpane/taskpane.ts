b5c74b34b0b7011451c2aa15607430dc755d9407
Sylvia Okafor <sokafor@microsoft.com>
2020-05-04 11:29:33 -0700

    Update taskpane.ts

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -10,7 +10,6 @@ Office.onReady(info => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
   }
 });
 
